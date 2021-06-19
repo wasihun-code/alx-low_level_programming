02e17d6fba1a2f79263e50177b924c56ea26e4f9
wasihun-code <wasihunageru7947@gmail.com>
2021-06-19 01:25:41 -0700

    betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints a text to standard output
  * Return: returns 0 since it is void
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	return(0);
 }
