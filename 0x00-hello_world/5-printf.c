3d38d921b3dc8612cfc3e6ff60fbb79f0159b60c
wasihun-code <wasihunageru7947@gmail.com>
2021-06-19 00:02:10 -0700

    added documentation

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
+/* This is documentation*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-	/* a documentation for this code is not necessary*/
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
