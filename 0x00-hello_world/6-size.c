1353ae9544a5c939976f1d952186bb658ff471fc
wasihun-code <wasihunageru7947@gmail.com>
2021-06-19 02:14:55 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints sizeof different data structures in c
+ * Return - returns 0 since void
+ */
+
+int main(void)
+{
+	fst = "byte(s)"
+	int i;
+	long l;
+	double d;
+	char c;
+	printf("Size of a char: %lu.\n" , (unsigned long)sizeof(c) , "byte(s)");
+	printf("Size of an int: %lu.\n" , (unsigned long)sizeof(i) , "byte(s)");
+	printf("Size of a long int: %lu.\n" , (unsigned long)sizeof(l) , "%c" ); 
+	return(0);
+}
