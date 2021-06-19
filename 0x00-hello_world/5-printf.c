bd87714e732b86256468bda76193a5e81bc94b7e
wasihun-code <wasihunageru7947@gmail.com>
2021-06-19 00:17:06 -0700

    documentation betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
-/* Program: printf
-   Author: wasihun */
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * function: main
+ * with: printf
+ * prints something
+ */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
