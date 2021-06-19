97226b563789c4b087bad89c43afd15e1a362c30
wasihun-code <wasihunageru7947@gmail.com>
2021-06-19 00:10:35 -0700

    stderr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf(stderr, "with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
