91e1dee3a8593a04f0831f50ad453c5158d5f69a
wasihun-code <wasihunageru7947@gmail.com>
2021-06-20 04:42:46 -0700

    printf is aza

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	fprintf(stderr, "with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
