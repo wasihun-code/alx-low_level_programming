1b56a41a5f856fd3bf21762393e11e0ed97f4a07
wasihun-code <wasihunageru7947@gmail.com>
2021-06-20 03:35:19 -0700

    documentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main: prints sizeof different data structures in c.
+ * main - prints sizeof different data structures in c.
  * Return: 0 since void main function.
  */
 
@@ -15,10 +15,10 @@ int main(void)
 	long long int s;
 	float f;
 
-	printf("Size of a char: %lu %s\n", (unsigned long)sizeof(c),str);
-	printf("Size of an int: %lu %s\n", (unsigned long)sizeof(l),str);
-	printf("Size of a long int: %lu %s\n", (unsigned long)sizeof(e),str);
-        printf("Size of a long long int: %lu %s\n", (signed long)sizeof(s),str);
-	printf("Size of a float: %lu %s\n", (unsigned long)sizeof(f),str);
+	printf("Size of a char: %lu %s\n", (unsigned long)sizeof(c), str);
+	printf("Size of an int: %lu %s\n", (unsigned long)sizeof(l), str);
+	printf("Size of a long int: %lu %s\n", (unsigned long)sizeof(e), str);
+	printf("Size of a long long int: %lu %s\n", (signed long)sizeof(s), str);
+	printf("Size of a float: %lu %s\n", (unsigned long)sizeof(f), str);
 	return (0);
 }
