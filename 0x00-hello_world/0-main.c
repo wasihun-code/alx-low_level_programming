04bffacc3eac490125d81a206735baf3dca8ffd5
wasihun-code <wasihunageru7947@gmail.com>
2021-06-20 04:40:20 -0700

    executable compilation

---
+++ 0x00-hello_world/0-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+ * main - just a function
+ * Return: 0(Always success)
+ */
+
+int main(void)
+{
+	printf("just a print");
+	return(0);
+}
---
+++ 0x00-hello_world/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+ * main - prints something.
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("print something");
+	return (0);
+}
