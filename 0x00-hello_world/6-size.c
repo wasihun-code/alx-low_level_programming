08db2d96d836fb28f4cbbac694140a0ae74d7405
wasihun-code <wasihunageru7947@gmail.com>
2021-06-20 02:02:49 -0700

    Betty is always making it hard

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 
 /**
- * main - prints sizeof different data structures in c
- * Return - returns 0 since void
+ * main - prints sizeof different data structures in c.
+ * @c - a character.
+ * @l - an integer.
+ * @e - a long integer.
+ * @s - a long long integer.
+ * @f - a float or a decimal number.
+ * @str - an array of characters.
+ * Return - returns 0 since void main function.
  */
 
 int main(void)
@@ -14,10 +20,10 @@ int main(void)
 	long e;
 	long long int s;
 	float f;
-	printf("Size of a char: %lu %s\n" , (unsigned long)sizeof(c), str);
-	printf("Size of an int: %lu %s\n" , (unsigned long)sizeof(l), str);
-	printf("Size of a long int: %lu %s\n" , (unsigned long)sizeof(e), str);
-        printf("Size of a long long int: %lu %s\n" , (signed long)sizeof(s), str);
-	printf("Size of a float: %lu %s\n" , (unsigned long)sizeof(f), str);
-	return(0);
+	printf("Size of a char: %lu %s\n",(unsigned long)sizeof(c),str);
+	printf("Size of an int: %lu %s\n",(unsigned long)sizeof(l),str);
+	printf("Size of a long int: %lu %s\n",(unsigned long)sizeof(e),str);
+        printf("Size of a long long int: %lu %s\n",(signed long)sizeof(s),str);
+	printf("Size of a float: %lu %s\n",(unsigned long)sizeof(f),str);
+	return (0);
 }
