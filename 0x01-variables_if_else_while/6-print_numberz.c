cf1e74d93ddca6e001cec84110e6cd1acf53263d
wasihun-code <wasihunageru7947@gmail.com>
2021-06-21 12:33:35 -0700

    tebahpla

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,11 +9,16 @@
 
 int main(void)
 {
-	int i;
-
+	int i = 0;
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d", i);
+		putchar(i);
 	}
+	putchar('\n');
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - tebahpla
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
