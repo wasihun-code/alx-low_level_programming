ae30928904674db049bbabe800f4035a04c84d75
wasihun-code <wasihunageru7947@gmail.com>
2021-06-23 06:27:52 -0700

    d

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,38 +2,22 @@
 #include <ctype.h>
 
 /**
- * threemain - Entry
- * _islower() - documentation
+ * _islower - documentation
  * Return: 0
  */
-
-int threemain()
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
 
 int _islower(int c)
 {
 	int results;
-	results = _islower(c);
 
-	if (results != 0)
+	results = _islower(c);
+	if (results >= 97 && results <= 122)
 	{
-		results = 1;
+		results == 1;
 	}
 	else
 	{
-		results = 0;
+		results == 0;
 	}
-
 	return (results);
 }
