0097a359f13f50f085e005ebe0b1a91505c56f30
wasihun-code <wasihunageru7947@gmail.com>
2021-06-23 04:31:12 -0700

    now really half way

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,5 +7,31 @@
 
 int main(void)
 {
+	int r;
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
 	return (0);
+}
+
+int print_last_digit(int n)
+{
+	int result;
+	if (n > 0)
+	{
+		result = n % 10;
+	}
+	else if (n < 0)
+	{
+		n = n * -1;
+		result = n % 10;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
 }
