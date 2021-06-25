e10f7ba6951947c72481d1a28d274da7ae6da416
wasihun-code <wasihunageru7947@gmail.com>
2021-06-25 04:26:57 -0700

    2 or 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - zero to ten.
+ * @i: index for stdout.
+ * Return: 0 since void.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
