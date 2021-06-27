4d996c16186c533edaaa03e187400a2a867e9619
wasihun-code <wasihunageru7947@gmail.com>
2021-06-27 10:06:56 -0700

    2

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - prints a triangle using #
+ * @n: no of lines of triangle.
+ * Return: 0 on success.
+ */
+
+void_print_triangle(int n)
+{
+	int i, j, k;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (k = n - 1; k > 0; k--)
+		{
+			printf(" ");
+		}
+		for (j = 1; j <= i; j++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+	return (0);
+}
