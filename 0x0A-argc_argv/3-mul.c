ea76e27e8d1e69928f6037a595193db3a6acba96
wasihun-code <wasihunageru7947@gmail.com>
2021-07-12 08:21:40 -0700

    yes

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - prints multiplication of two integers.
+ * @argc: no of command line arguments.
+ * @argv: arrays of string of command line arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+}
