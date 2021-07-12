5ab6ba1c174fc7e99c9b8467108d35fc1f1b5fa0
wasihun-code <wasihunageru7947@gmail.com>
2021-07-12 11:33:12 -0700

    to

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,12 @@ int main(int argc, char *argv[])
 	if (argc == 1)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 	else
+	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
 
 	return (0);
 }
