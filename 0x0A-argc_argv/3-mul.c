17f55bf84ce47c6c260b86da07cb537a5a8321fa
wasihun-code <wasihunageru7947@gmail.com>
2021-07-12 11:28:30 -0700

    why shouldn't i

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,12 +7,15 @@
  * @argc: no of command line arguments.
  * @argv: arrays of string of command line arguments.
  * Return: 0.
-*/
+ */
 
 int main(int argc, char *argv[])
 {
 	if (argc == 1)
+	{
 		printf("Error\n");
+		return 1;
+	}
 	else
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
