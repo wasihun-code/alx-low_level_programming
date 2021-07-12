c0ad36eaed402f898abbf799bb0569e953f0745c
wasihun-code <wasihunageru7947@gmail.com>
2021-07-12 06:50:55 -0700

    why why

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main -prints command line arguments
+ * @argc: no of commandline arguments
+ * @argv: string of arrays of command line arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
