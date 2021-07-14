ee9c3adec8ab9255b4780b4dc7b74a5945342939
wasihun-code <wasihunageru7947@gmail.com>
2021-07-14 08:48:26 -0700

    why commit

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * create_array - creates an array of characters and initialize it.
+ * @size: the size of the array.
+ * @c: initializing character.
+ * Return: pointer to a cahracter.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char  *b = (char *)malloc(size * sizeof(char));
+
+	*b = c;
+
+	return (b);
+}
