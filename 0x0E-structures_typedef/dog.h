5d021edc7b5273ffe6a4043c743bb19efb8c737e
wasihun-code <wasihunageru7947@gmail.com>
2021-07-19 14:29:31 -0700

    this is for betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,15 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - this is a dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog is.
+ */
 
-typedef struct dog {
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
