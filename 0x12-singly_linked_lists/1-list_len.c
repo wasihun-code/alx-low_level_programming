9488fe97b32192318cfa3716cf8e9cebd4057fce
wasihun-code <wasihunageru7947@gmail.com>
2021-07-29 11:43:53 -0700

    return no of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints values in nodes.
+ * @h: no of nodes.
+ * Return: No of nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
