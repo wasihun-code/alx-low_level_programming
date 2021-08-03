1325453e1311ee12ed4f2035d22818c238827d26
wasihun-code <wasihunageru7947@gmail.com>
2021-08-03 15:36:32 -0700

    why not

--- 0x13-more_singly_linked_lists/5-main.c
+++ 0x13-more_singly_linked_lists/5-main.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 	listint_t *head;
+	int sum;
 
 	head = NULL;
 	add_nodeint_end(&head, 0);
@@ -16,8 +17,8 @@ int main(void)
 	add_nodeint_end(&head, 98);
 	add_nodeint_end(&head, 402);
 	add_nodeint_end(&head, 1024);
-	print_listint(head);
+	sum = sum_listint(head);
+	printf("sum = %d\n", sum);
 	free_listint2(&head);
-	printf("%p\n", (void *)head);
 	return (0);
 }
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - returns sum of a linkedlist
+ * @head: points to head of node
+ * Returns: integer.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head!=NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
