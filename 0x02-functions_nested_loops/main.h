3d122ab8011a4a52cb834f1e3fb812150b116d8e
wasihun-code <wasihunageru7947@gmail.com>
2021-08-23 10:54:57 +0300

    main and holberton header files are having a conflict

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -10,3 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
 int add (int, int);
+
+#endif
