5a9df28974ce549d3ca73eab43dc78254bb190e1
wasihun-code <wasihunageru7947@gmail.com>
2021-08-23 10:50:37 +0300

    ufa

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,23 +7,23 @@
  * Return: 0 on success.
  */
 
- void print_triangle(int size)
- {
- 	int row, col, col2;
+void print_triangle(int size)
+{
+	int row, col, col2;
 
- 	if (size <= 0)
- 		_putchar('\n');
+	if (size <= 0)
+		_putchar('\n');
 
- 	for (row = 1; row <= size; row++)
- 	{
- 		for (col = row; col < size; col++)
- 		{
- 			_putchar(' ');
- 		}
- 		for (col2 = 1; col2 <= row; col2++)
- 		{
- 			_putchar('#');
- 		}
- 		_putchar('\n');
- 	}
- }
+	for (row = 1; row <= size; row++)
+	{
+		for (col = row; col < size; col++)
+		{
+			_putchar(' ');
+		}
+		for (col2 = 1; col2 <= row; col2++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
