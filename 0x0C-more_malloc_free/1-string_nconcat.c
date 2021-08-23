abc2306270f688bf96240eacb643bfb53369d444
wasihun-code <wasihunageru7947@gmail.com>
2021-08-23 14:01:55 +0300

    why not full

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,60 @@
 #include "holberton.h"
 
 /**
-  * string_nconcat - concatenate two strings
-  * @s1: first string
-  * @s2: second string
-  * @n: number of bytes to be added to s1
-  * Return: the concatet string
-  */
+ * _strlen - returns length of a string
+ * @s: string passed
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*(s + l))
+		l++;
+	return (l);
+}
+
+/**
+ * string_nconcat - concatenates 2 strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * @n: number of byties for s2
+ * Return: pointer to a new string
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int j, k, sz, l1, l2;
-	char *str;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int lens1 = 0;
+	unsigned int lens2 = 0;
+	char *fulllength;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (l1 = 0; s1[l1] != '\0'; l1++)
-		;
-	for (l2 = 0; s2[l2] != '\0'; l2++)
-		;
-
-	if (n >= l2)
-		n = l2;
-	sz = l1 + n + 1;
-
-	str = malloc(sizeof(char) * sz);
-	if (str == NULL)
-		return (NULL);
-
-	for (j = 0; j < l1; j++)
-		str[j] = s1[j];
-	for (k = 0; k < n; k++)
 	{
-		str[j] = s2[k];
-		j++;
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	lens1 = _strlen(s1);
+	lens2 = _strlen(s2);
+	if (lens2 > n)
+	{
+		lens2 = n;
 	}
 
-	str[j] = '\0';
-	return (str);
+	fulllength = malloc((lens1 + lens2) * sizeof(char) + 1);
+	for (i = 0; i < lens1; i++)
+	{
+		fulllength[i] = s1[i];
+	}
+	for (j = 0; j < lens2; i++, j++)
+	{
+		fulllength[i] = s2[j];
+	}
+	fulllength[i] = '\0';
+	return (fulllength);
 }
