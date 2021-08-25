4dc3640154021a4a9d00a43cf007a48ab0dd547e
wasihun-code <wasihunageru7947@gmail.com>
2021-08-25 14:44:20 +0300

    memory setting

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
-
 /**
- * _memset - allocates c to memory.
- * @s: points to an array of characters.
- * @b: character to fill.
- * @n: how many adress to fill.
- * Return: a character.
+ *_memset-fill memory with a constant byte.
+ *@s:pointer to the memory area.
+ *@b:Constant byte to fill the memory area.
+ *@n:Number of bytes to fill.
+ *Return:Pointer to the memory area s.
  */
+
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i; /* looping var */
+	unsigned int i;
+	char *p = (char *)s;
 
 	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+	{
+		p[i] = b;
+	}
+	return (p);
 }
