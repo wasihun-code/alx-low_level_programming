84450c2584d681da764a5f8b9eb42228e8cd6c05
wasihun-code <wasihunageru7947@gmail.com>
2021-08-25 14:38:41 +0300

    strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,41 @@
 #include "holberton.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
-  * string_nconcat - concatenate two strings
-  * @s1: first string
-  * @s2: second string
-  * @n: number of bytes to be added to s1
-  * Return: the concatet string
-  */
-
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string.
+ * @s2: pointer to 2nd string.
+ * @n: Number of bytes from n2 to concatenate.
+ *
+ * Return:Pointer to space in memory containing concatenated string.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int j, k, sz, l1, l2;
 	char *str;
+	unsigned int i, j, s1_length, s2_length;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (l1 = 0; s1[l1] != '\0'; l1++)
-		;
-	for (l2 = 0; s2[l2] != '\0'; l2++)
-		;
 
-	if (n >= l2)
-		n = l2;
-	sz = l1 + n + 1;
-
-	str = malloc(sizeof(char) * sz);
+	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+		;
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+		;
+	str = malloc(s1_length + n + 1);
 	if (str == NULL)
-		return (NULL);
-
-	for (j = 0; j < l1; j++)
-		str[j] = s1[j];
-	for (k = 0; k < n; k++)
 	{
-		str[j] = s2[k];
-		j++;
+		return (NULL);
 	}
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
 
-	str[j] = '\0';
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
 	return (str);
 }
