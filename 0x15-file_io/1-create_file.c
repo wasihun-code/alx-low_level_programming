33b37d2e18325504055d0f65d0f9095b56e4c5c2
wasihun-code <wasihunageru7947@gmail.com>
2021-08-25 14:03:26 +0300

    towards glory

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,25 +1,28 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * create_file - creates a file.
- * @filename: name of file to be created.
- * @text_content: a Null terminated string to be written.
- * Return: integer.
+ * create_file - creates a file
+ * @filename: pointer to filename
+ * @text_content: string that is appended to file
+ * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	register int fd, w, len = 0;
 
-	if (text_content == NULL || filename == NULL)
+	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-	if (fd < 0)
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-
-	write(fd, text_content, sizeof(text_content));
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
 	close(fd);
-	return (fd - 2);
+	return (1);
 }
