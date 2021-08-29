1103fdd0e0ad96800b7e8214dfeca5d2aac1f272
wasihun-code <wasihunageru7947@gmail.com>
2021-08-29 16:32:20 +0300

    to you

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,26 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * get_bit - returns a bit at a given index.
- * @n: integer to be converted to binary.
- * @index: index for returning the bit.
- * Return: index of a bit.
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int  i = 0, j, binaryNum[1000];
-
-	if (n == 0)
-		return (0);
-	while (n >= 1)
-	{
-		binaryNum[i] = n % 2;
-		n = n / 2;
-		i++;
-	}
-	j = sizeof(binaryNum) / sizeof(unsigned int);
-	if (j < index)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	return (binaryNum[index]);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
