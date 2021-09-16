cac9ef32299dbcedade42cacc5650e2d0e63c723
wasihun-code <wasihunageru7947@gmail.com>
2021-09-16 17:22:26 +0300

    Back to see with doubly linked lists

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a node at beginning of DLL.
+ * @head: pointer to transverse.
+ * @n: data to be inserted to the new node.
+ * Return: the address of the new node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+	temp->n = n;
+	temp->next = (*head);
+	temp->prev = NULL;
+
+	if ((*head) != NULL)
+		(*head)->prev = temp;
+	(*head) = temp;
+
+	return (temp);
+}
