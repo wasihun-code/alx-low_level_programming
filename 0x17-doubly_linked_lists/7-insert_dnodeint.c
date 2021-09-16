7ba8e23e71a2f40fdf74330494bebc5bf5c892f5
wasihun-code <wasihunageru7947@gmail.com>
2021-09-17 00:27:07 +0300

    fixed betty style error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,32 +1,38 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert the node at index
- * @head: pointer to transverse
- * @index: index of the node.
- * Return: address of index of node.
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ * Return: NULL or new
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int len = 0;
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *last = *h;
+	dlistint_t *tmp = *h, *new;
 
-	while (last != NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
 	{
-		if (len == idx)
-			break;
-		last = last->next;
-		len++;
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
 	}
 
-	new_node->n = n;
-	new_node->next = last->next;
-	last->next = new_node;
-	new_node->prev = last;
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-	if (new_node->next != NULL)
-		new_node->next->prev = new_node;
-	return (new_node);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
