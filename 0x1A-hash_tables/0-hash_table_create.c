785d6f0e4098caa4d52d90f83161fa33b93fc1f6
wasihun-code <wasihunageru7947@gmail.com>
2021-10-10 00:43:35 +0530

    Betty was replaced by pep8 and now I forgot

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,18 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-    if (table == NULL)
-	return NULL;
+	if (table == NULL)
+		return (NULL);
 
-    table->size = size;
+	table->size = size;
+	table->array = calloc(table->size, sizeof(size));
 
-    table->array = calloc(table->size, sizeof(size));
-    if (table->array == NULL)
-    {
-	free(table);
-	return NULL;
-    }
-    return table;
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	return (table);
 }
