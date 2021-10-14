d7857602e8b0218d3608e7de1da9e2b8d5e2a482
wasihun-code <wasihunageru7947@gmail.com>
2021-10-14 08:06:14 +0530

    to

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "../hash_tables.h"
+#include "hash_tables.h"
 
 /**
  * main - check the code for Holberton School students.
@@ -13,6 +13,6 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	printf("%p\n", (void *)ht);
+	hash_table_set(ht, "betty", "holberton");
 	return (EXIT_SUCCESS);
 }
